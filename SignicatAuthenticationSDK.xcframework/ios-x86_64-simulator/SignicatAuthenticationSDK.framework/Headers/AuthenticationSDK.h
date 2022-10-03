15753bd2e35191301b638069bcbb093eeae5efb9
Bartosz Swiety <contact@bartoszswiety.com>
2022-10-04 00:09:09 +0200

    v1.1.1

---
+++ SignicatAuthenticationSDK.xcframework/ios-x86_64-simulator/SignicatAuthenticationSDK.framework/Headers/AuthenticationSDK.h
@@ -0,0 +1,18 @@
+//
+//  AuthenticationSDK.h
+//  AuthenticationSDK
+//
+//  Created by tmx on 10/05/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AuthenticationSDK.
+FOUNDATION_EXPORT double AuthenticationSDKVersionNumber;
+
+//! Project version string for AuthenticationSDK.
+FOUNDATION_EXPORT const unsigned char AuthenticationSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AuthenticationSDK/PublicHeader.h>
+
+
